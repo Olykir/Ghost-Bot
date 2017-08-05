bd8d14a589131a0e4efe9335e0aba5f7ba80b248
Olykir <oillicker806@gmail.com>
2017-08-04 19:06:14 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,8 +1,6 @@
-  const Status = ["Im a ghost", "Boo", "Scary right?","New Update!"];
- const Discord = require(`discord.js`)
+ const Status = ["Im a ghost", "Boo", "Scary right?","New Update!"];
+ const Discord = require("discord.js");
 module.exports = (client, message) => {
- 
-
 client.user.setGame(Status[Math.floor(Math.random() * Status.length)],"https://www.twitch.tv/olykir");
-client.user.setStatus('online');
-};
+client.user.setStatus("online");
+}
